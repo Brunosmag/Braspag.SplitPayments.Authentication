5cf6047148d0a327fa89b92d54dcde1d2bd0c006
Bruno Magalhães <brunosmag@outlook.com>
2020-08-27 20:38:37 -0300

    Injetando as dependências de uma maneira mais fácil.

--- Braspag.Authentication/Braspag.Authentication.Commons/DependencyInjection/BraspagAuthenticationCollectionExtensions.cs
+++ Braspag.Authentication/Braspag.Authentication.Commons/DependencyInjection/BraspagAuthenticationCollectionExtensions.cs
@@ -1,15 +1,20 @@
 ﻿using Braspag.Authentication.Domain.Services.Base64Encrypters;
+using Braspag.Authentication.Domain.Services.BraspagTokenOrchestrator;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Braspag.Authentication.Commons.DependencyInjection
 {
     public static class BraspagAuthenticationCollectionExtensions
     {
-        public static void AddBraspagAuthentication(this IServiceCollection services)
+        public static IServiceCollection AddBraspagAuthentication(this IServiceCollection services)
         {
             services.AddHttpClient();
 
             services.AddTransient<IBase64Encrypter, Base64Encrypter>();
+
+            services.AddTransient<IBraspagTokenOrchestrator, BraspagTokenOrchestrator>();
+
+            return services;
         }
     }
 }
