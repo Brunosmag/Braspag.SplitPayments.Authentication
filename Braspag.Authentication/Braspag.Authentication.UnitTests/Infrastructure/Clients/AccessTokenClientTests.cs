cbe63bd9c1c1d0658b1640e6e2fa9b91834a8b97
Bruno Magalhães <brunosmag@outlook.com>
2021-06-15 15:22:14 -0300

    Adicionando teste para AccessTokenClient.

---
+++ Braspag.Authentication/Braspag.Authentication.UnitTests/Infrastructure/Clients/AccessTokenClientTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture.Idioms;
+using Braspag.Authentication.Infrastructure.Clients;
+using Braspag.Authentication.Infrastructure.Contracts;
+using Braspag.Authentication.UnitTests.AutoFixture;
+using Braspag.Authentication.UnitTests.NSubstitute;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NSubstitute;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Braspag.Authentication.UnitTests.Infrastructure.Clients
+{
+    public class AccessTokenClientTests
+    {
+        [Theory,AutoNSubstituteData]
+        public void Sut_Should_Guard_Its_Clause(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(AccessTokenClient).GetConstructors());
+        }
+
+        [Theory,AutoNSubstituteData]
+        public void Sut_Should_Implement_IAccessTokenClient(AccessTokenClient sut)
+        {
+            Assert.IsAssignableFrom<IAccessTokenClient>(sut);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public async Task Sut_Should_RequestAsync_Correctly_When_Sandbox(
+            string clientCredentialsInBase64,
+            AccessToken sandboxAccessToken,
+            AccessTokenClient sut)
+        {
+            var fakeHttpMessageWrapper = new FakeHttpMessageWrapper
+            {
+                HttpContent = new StringContent(JsonConvert.SerializeObject(sandboxAccessToken))
+            };
+
+            var httpClient = new HttpClient(fakeHttpMessageWrapper) { BaseAddress = new Uri("https://authsandbox.braspag.com.br") };
+            sut.HttpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+
+            var token = await sut.GetSandboxTokenAsync(clientCredentialsInBase64);
+
+            sut.HttpClientFactory.Received().CreateClient();
+
+            token.Should().BeEquivalentTo(sandboxAccessToken);
+        }
+    }
+}
---
+++ Braspag.Authentication/Braspag.Authentication.UnitTests/NSubstitute/FakeHttpMessageWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Braspag.Authentication.UnitTests.NSubstitute
+{
+    public class FakeHttpMessageWrapper : HttpMessageHandler
+    {
+        public HttpContent HttpContent { get; set; }
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage { StatusCode = HttpStatusCode.OK, Content = HttpContent });
+        }
+    }
+}
