cfd54bba813634a44be5997d5569da63d4dc0c50
Bruno Magalhães <brunosmag@outlook.com>
2021-06-18 16:55:01 -0300

    Removendo divisão por multiplos projetos.

---
+++ Braspag.Authentication/Application/Services/Base64Encrypters/IBase64Encrypter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Braspag.Authentication.Application.Services.Base64Encrypters
+{
+    public interface IBase64Encoder
+    {
+        string EncodeInBase64(Guid clientId, string clientSecret);
+    }
+}
---
+++ Braspag.Authentication/Infrastructure/Clients/IAccessTokenClient.cs
@@ -0,0 +1,12 @@
+﻿using Braspag.Authentication.Infrastructure.Contracts;
+using System.Threading.Tasks;
+
+namespace Braspag.Authentication.Infrastructure.Clients
+{
+    public interface IAccessTokenClient
+    {
+        Task<AccessToken> GetSandboxTokenAsync(string clientCredentialsInBase64);
+
+        Task<AccessToken> GetProductionTokenAsync(string clientCredentialsInBase64);
+    }
+}
