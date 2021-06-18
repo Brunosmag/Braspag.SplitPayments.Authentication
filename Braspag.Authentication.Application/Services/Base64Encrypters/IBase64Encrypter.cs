b708756c5248e263498b2b68ef7598af3b808b2d
Bruno Soares Magalhaes <bmagalhaes@braspag.com.br>
2021-06-18 17:09:16 -0300

    Limpando pastas após remoção de projetos.

--- Braspag.Authentication.Application/Services/Base64Encrypters/IBase64Encrypter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Braspag.Authentication.Application.Services.Base64Encrypters
-{
-    public interface IBase64Encoder
-    {
-        string EncodeInBase64(Guid clientId, string clientSecret);
-    }
-}
